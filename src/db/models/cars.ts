a64fbe0dcf020edd3c84fb0039fce96473e7f068
Lucas Stecher <lucasvieira0070@gmail.com>
2025-02-18 21:28:02 -0300

    :sparkles: feat: include tables and migrations

--- src/db/models/cars.ts
+++ src/db/models/cars.ts
@@ -1,9 +1,22 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { pgTable, varchar, timestamp, numeric, uuid, integer, pgEnum } from "drizzle-orm/pg-core";
+
+export const fuelTypes = pgEnum("fuel_type", ["gasoline", "hybrid", "electric"]);
+export const transmissionTypes = pgEnum("transmission", ["manual", "automatic"]);
+export const carStatus = pgEnum("status", ["available", "sold", "reserved"]);
 
 export const carsTable = pgTable("cars", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
+  id: uuid().primaryKey(),
+  model: varchar({ length: 255 }).notNull(),
   brand: varchar({ length: 255 }).notNull().unique(),
-  year: varchar({ length: 4 }).notNull(),
-  mileage: varchar({ length: 255 }).notNull()
+  year: integer().notNull(),
+  price: numeric().notNull(),
+  mileage:integer().notNull().default(0),
+  fuel_type: fuelTypes().notNull(),
+  transmission: transmissionTypes().notNull(),
+  status: carStatus().notNull(),
+  picture: varchar({ length: 255 }).notNull(),
+  color: varchar({ length: 30 }).notNull(),
+  created_at: timestamp().defaultNow(),
+  updated_at: timestamp().$onUpdate(()=> new Date()),
+  deleted_at: timestamp()
 });
---
+++ src/db/repository/cars.repository.ts
@@ -0,0 +1,23 @@
+
+import { db } from "src/lib/drizzle";
+export class carsRepository {
+  constructor() {
+    
+  }
+
+  public async createCar(input) {
+
+  }
+
+  public async updateCar(input) {
+
+  }
+
+  public async getCar(id) {
+
+  }
+
+  public async deleteCar(id) {
+
+  }
+}
